85ed0044c64f55236e113d9d2aca6aba09285ed9
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-23 14:14:21 -0300

    Added UpdateCrypto endpoint and refactored ReadCrypto.

--- server/updateCrypto.go
+++ server/updateCrypto.go
@@ -7,14 +7,25 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	pb "github.com/lucassauro/klever-challenge/proto"
 )
 
-func (s *Server) ReadCrypto(ctx context.Context, req *pb.CryptoId) (*pb.Crypto, error) {
+func (s *Server) UpdateCrypto(ctx context.Context, req *pb.Crypto) (*pb.CryptoId, error) {
 	id := bson.M{"_id": req.Id}
 
-	res := MongoCollection.FindOne(ctx, id)
+	updateTo := bson.M{
+		"name": req.Name,
+		"short": req.Short,
+		"votes": req.Votes,
+	}
+
+	res := MongoCollection.FindOneAndUpdate(
+		ctx, 
+		id, bson.M{"$set": updateTo}, 
+		options.FindOneAndUpdate().SetReturnDocument(1),
+	)
 
 	coin := &Coin{}
 
@@ -22,10 +33,7 @@ func (s *Server) ReadCrypto(ctx context.Context, req *pb.CryptoId) (*pb.Crypto, 
 		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("No coin with specified Id. %v", err))
 	}
 
-	return &pb.Crypto{
+	return &pb.CryptoId{
 		Id: coin.Id,
-		Name: coin.Name,
-		Short: coin.Short,
-		Votes: coin.Votes,
 	}, nil
 }
