8b91a7aaf486743cf5b42072b40d726f3f4c70eb
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-24 14:39:51 -0300

    Refactored and implemented error handling on createCrypto.go.

--- server/createCrypto.go
+++ server/createCrypto.go
@@ -7,28 +7,45 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/go-playground/validator/v10"
 	pb "github.com/lucassauro/klever-challenge/proto"
 )
 
 // CreateCrypto receives context and request with NewCrypto struct as parameters, create a coin in the database and return its auto incremented integer Id.
 func (s *Server) CreateCrypto(ctx context.Context, req *pb.NewCrypto) (*pb.CryptoId, error) {
+	
+	lastId, Err := FindLastId()
+
+	if Err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintln(Err))
+	}
+
 	// Following variable stores the Id of the last inserted document plus one.
-	coinId := uint32(FindLastId() + 1)
+	generatedCoinId := uint32(lastId + 1)
 	
 	newCoin := &Coin {
-		Id: coinId,
+		Id: generatedCoinId,
 		Name: req.Name,
 		Short: req.Short,
 		Votes: 0,
 	}
 	
+	// validate existance of both name and short 
+	validate = validator.New()
+
+	validationErr := validate.Struct(newCoin)
+	
+	if validationErr != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintln(validationErr))
+	}
+
 	_, err := MongoCollection.InsertOne(ctx, newCoin)
 
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+		return nil, status.Errorf(codes.Internal, fmt.Sprintln(err))
 	}
 
 	return &pb.CryptoId{
-		Id: coinId,
+		Id: generatedCoinId,
 	}, nil
 }
