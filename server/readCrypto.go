29408c0b7d06fab1b1cc0b260d23d627022f361d
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-24 14:40:04 -0300

    Refactored and implemented error handling on readCrypto.go.

--- server/readCrypto.go
+++ server/readCrypto.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	pb "github.com/lucassauro/klever-challenge/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // ReadCrypto function receives context and request with id as parameters, return a crypto if it exists and an error.
 func (s *Server) ReadCrypto(ctx context.Context, req *pb.CryptoId) (*pb.Crypto, error) {
-	
-	coin := DoesThisCryptoExist(req.Id)
+	coin, err := DoesThisCryptoExist(req.Id)
 
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintln(err))
+	}
+	
 	return &pb.Crypto{
 		Id: coin.Id,
 		Name: coin.Name,
