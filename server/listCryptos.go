63c05293fd01f44bffb7c09111c6d84fa157409b
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-24 13:34:59 -0300

    Added listCryptos endpoint.

---
+++ server/listCryptos.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	pb "github.com/lucassauro/klever-challenge/proto"
+)
+
+// ListCryptos receives empty and stream as parameters. Returns a stream with all cryptos found in mongo collection.
+func (s *Server) ListCryptos(no *empty.Empty, stream pb.CryptoService_ListCryptosServer) error {
+	res, err := MongoCollection.Find(context.Background(), bson.D{})
+	if err != nil {
+		status.Errorf(codes.Internal, fmt.Sprintf("Error. %v", err))
+	}
+
+	defer res.Close(context.Background())
+	
+	coin := &Coin{}
+
+	for res.Next(context.Background()) {
+		err := res.Decode(coin)
+		if err != nil {
+			status.Errorf(codes.Internal, fmt.Sprintf("Error. %v", err))
+		}
+		stream.Send(&pb.Crypto{
+			Id: coin.Id,
+			Name: coin.Name,
+			Short: coin.Short,
+			Votes: coin.Votes,
+		})
+	}
+
+	if err := res.Err(); err != nil {
+		status.Errorf(codes.Internal, fmt.Sprintf("Error. %v", err))
+	}
+
+	return nil
+}
