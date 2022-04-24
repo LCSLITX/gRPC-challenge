be80380bcf4ec789219c3c785e0fa52fe72f9ae9
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-24 14:39:28 -0300

    Refactored and implemented error handling on common.go.

--- server/common.go
+++ server/common.go
@@ -13,11 +13,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	
 	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo/options"	
+
 )
 
 // global MongoCollection incialization
 var MongoCollection *mongo.Collection
+
+// global validator inicialization
+var validate *validator.Validate
 
 // global gotenv inicialization.
 func init() {
@@ -39,16 +44,17 @@ type Votes struct {
 // ===== Helper functions =====
 
 // FindLastId function finds the Id of the last added document in a mongo collection.
-func FindLastId() uint32 {
+func FindLastId() (uint32, error) {
 	opts := options.FindOne()
 	opts.SetSort(bson.D{{Key: "_id", Value: -1}})
 	filter := bson.D{}
 	lastId := MongoCollection.FindOne(context.Background(), filter, opts)
 	data := &Coin{}
+
 	if err := lastId.Decode(data); err != nil {
-		status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+		return 0, status.Errorf(codes.Internal, fmt.Sprintln(err))
 	}
-	return uint32(data.Id)
+	return uint32(data.Id), nil
 }
 
 // FrankensteinGetVotesPls function receives a parameter of type bson.M, returned by mongo.Watch(), and iterates over it until it get number of votes to return it.
@@ -72,16 +78,21 @@ func FrankensteinGetVotesPls(data bson.M) int64 {
 }
 
 // DoesThisCryptoExist function receives parameter of type *pb.CryptoId, search mongo collection for it and return info Coin struct.
-func DoesThisCryptoExist(id uint32) (*Coin) {
+func DoesThisCryptoExist(id uint32) (*Coin, error) {
+	
 	filter := bson.M{"_id": id}
-
-	res := MongoCollection.FindOne(context.Background(), filter)
+	
+	res:= MongoCollection.FindOne(context.Background(), filter)
+	
+	if res.Err() != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintln(res.Err()))
+	}
 
 	coin := &Coin{}
 
 	if err := res.Decode(coin); err != nil {
-		status.Errorf(codes.NotFound, fmt.Sprintf("No coin with specified Id. %v", err))
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintln(err))
 	}
 
-	return coin
+	return coin, nil
 }
