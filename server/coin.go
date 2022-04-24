7e8ae38c2a1f5e4665db1ebf46e78d50fd5bbf88
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-24 13:33:37 -0300

    Added liveCryptoVotes endpoint and refactored common.go to concentrate helper functions.

--- server/coin.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type Coin struct {
-	Id 					uint32			`bson:"_id,omitempty"`
-	Name 				string 			`bson:"name"`
-	Short 			string 			`bson:"short"`
-	Votes 			int64 			`bson:"votes"`
-}
--- server/common.go
+++ server/common.go
@@ -1,20 +1,87 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"context"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	
 	"github.com/subosito/gotenv"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"	
 )
 
+// global MongoCollection incialization
 var MongoCollection *mongo.Collection
 
+// global gotenv inicialization.
 func init() {
 	gotenv.Load()
 }
 
-// var user string = os.Getenv("MONGO_USER")
-// var password string = os.Getenv("MONGO_PASSWORD")
-// var domain string = os.Getenv("MONGO_DOMAIN")
-var ConnectionString string = os.Getenv("MONGO_CONNECTION_STRING")
+// ===== Structs =====
+type Coin struct {
+	Id 					uint32			`bson:"_id,omitempty" json:"id,omitempty"`
+	Name 				string 			`bson:"name,omitempty" json:"name,omitempty" validate:"required"`
+	Short 			string 			`bson:"short,omitempty" json:"short,omitempty" validate:"required"`
+	Votes 			int64 			`bson:"votes,omitempty" json:"votes,omitempty"`
+}
+
+type Votes struct {
+	Votes 			int64 			`bson:"votes" json:"votes"`
+}
+
+// ===== Helper functions =====
+
+// FindLastId function finds the Id of the last added document in a mongo collection.
+func FindLastId() uint32 {
+	opts := options.FindOne()
+	opts.SetSort(bson.D{{Key: "_id", Value: -1}})
+	filter := bson.D{}
+	lastId := MongoCollection.FindOne(context.Background(), filter, opts)
+	data := &Coin{}
+	if err := lastId.Decode(data); err != nil {
+		status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+	}
+	return uint32(data.Id)
+}
+
+// FrankensteinGetVotesPls function receives a parameter of type bson.M, returned by mongo.Watch(), and iterates over it until it get number of votes to return.
+// know more: https://stackoverflow.com/questions/61354850/how-to-assert-a-primitive-m-to-a-mapstringstring
+func FrankensteinGetVotesPls(data bson.M) int64 {
+	a := make(map[string]interface{})
+	for key, value := range data["updateDescription"].(primitive.M) {
+		if key == "updatedFields" {
+			a["a"] = value
+		}
+	}
+	for key, value := range a["1"].(primitive.M) {
+		if key == "votes" {
+			a["a"] = value
+		}
+	}
+	// type assertion
+	t := a["a"].(int64)
+
+	return t
+}
+
+// DoesThisCryptoExist function receives parameter of type *pb.CryptoId, search mongo collection for it and return info Coin struct.
+func DoesThisCryptoExist(id uint32) (*Coin) {
+	filter := bson.M{"_id": id}
+
+	res := MongoCollection.FindOne(context.Background(), filter)
+
+	coin := &Coin{}
+
+	if err := res.Decode(coin); err != nil {
+		status.Errorf(codes.NotFound, fmt.Sprintf("No coin with specified Id. %v", err))
+	}
+
+	return coin
+}
