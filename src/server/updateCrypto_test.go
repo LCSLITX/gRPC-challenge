82a77eab68803947e8f0c86101cb9b8086758d16
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-25 06:14:52 -0300

    Added updateCrypto_test.go

---
+++ src/server/updateCrypto_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"google.golang.org/grpc"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/lucassauro/klever-challenge/src/proto"
+)
+
+func Test_UpdateCrypto(t *testing.T) {
+	ctx := context.Background()
+	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+	connection, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), credentials)
+	
+	if err != nil {
+	t.Fatalf("Error: %v", err)
+	}
+
+	defer connection.Close()
+
+	client := pb.NewCryptoServiceClient(connection)
+	
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+	defer mt.Close()
+
+	res, _ := client.CreateCrypto(ctx, &pb.NewCrypto{
+		Name: "Testo",
+		Short: "TVT",
+	})
+
+	mt.Run("Success", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			primitive.E	{ Key: "ok", Value: 1 },
+			primitive.E {
+				Key: "value", 
+				Value: bson.D {
+					primitive.E{ Key: "Name", Value: "Teste"},
+					primitive.E{ Key: "Short", Value: "TST"},
+			}}})
+
+		_, err := client.UpdateCrypto(context.Background(), &pb.Crypto{
+			Id: res.Id,
+			Name: "Teste",
+			Short: "TST",
+		})
+
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+	})
+}
