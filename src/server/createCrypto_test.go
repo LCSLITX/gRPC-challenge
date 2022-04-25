7f4434ff57d2aa686e5b882979ae392d76eef42f
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-25 01:06:53 -0300

    Added createCrypto_test.go and refactored server_test.go

---
+++ src/server/createCrypto_test.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/lucassauro/klever-challenge/src/proto"
+)
+
+// type testUtilities[T any] struct {
+// 	ctx emptyCtx;
+// 	credentials []T;
+// 	connection []T;
+// 	client []T;
+// 	mt []T;
+// }
+
+// var utilities = make(&testUtilities{})
+
+// func testUtils(t *testing.T) {
+// 	ctx := context.Background()
+// 	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+// 	connection, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), credentials)
+// 	// https://stackoverflow.com/questions/42102496/testing-a-grpc-service
+
+// 	if err != nil {
+// 	t.Fatalf("Error: %v", err)
+// 	}
+
+// 	client := pb.NewCryptoServiceClient(connection)
+
+// 	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+// 	return &testUtilities{
+// 		ctx,
+// 		credentials,
+// 		connection,
+// 		client,
+// 		mt,
+// 	}
+// }
+
+func TestCreateCrypto(t *testing.T) {
+	ctx := context.Background()
+	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+	connection, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), credentials)
+	// https://stackoverflow.com/questions/42102496/testing-a-grpc-service
+	
+	if err != nil {
+	t.Fatalf("Error: %v", err)
+	}
+
+	defer connection.Close()
+
+	client := pb.NewCryptoServiceClient(connection)
+	
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+	defer mt.Close()
+
+	mt.Run("Succeeded", func(mt *mtest.T) {
+		mt.AddMockResponses(mtest.CreateSuccessResponse())
+
+		if _, err := client.CreateCrypto(
+			ctx, 
+			&pb.NewCrypto{
+				Name: "Tests",
+				Short: "Tst",
+			}); err != nil {
+				t.Errorf("Error: %v", err)
+			}
+	})
+}
+
+// TestErrorCreateCrypto tests if the function CreateCrypto works properly.
+// https://stackoverflow.com/questions/42102496/testing-a-grpc-service
+func TestErrorCreateCrypto(t *testing.T) {
+	ctx := context.Background()
+	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+	connection, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), credentials)
+	
+	if err != nil {
+	t.Fatalf("Error: %v", err)
+	}
+
+	defer connection.Close()
+
+	client := pb.NewCryptoServiceClient(connection)
+	
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+	defer mt.Close()
+
+	mt.Run("Failure", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D { primitive.E { Key: "error", Value: 0 } })
+
+		_, err := client.CreateCrypto(ctx, &pb.NewCrypto{})
+		
+		if err == nil {
+			t.Errorf("Error expected")
+		}
+
+		stat, ok := status.FromError(err)
+
+		if !ok {
+			t.Error("Error expected")
+		}
+
+		if stat.Code() != codes.InvalidArgument {
+			t.Errorf("Expected InvalidArgument, got %v", stat.Code().String())
+		}
+	})
+}
--- src/server/server_test.go
+++ src/server/server_test.go
@@ -1,32 +1,33 @@
 package main
 
-import (
-	"context"
+import(
 	"log"
 	"net"
+	"context"
 
-	pb "github.com/lucassauro/klever-challenge/src/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
+	pb "github.com/lucassauro/klever-challenge/src/proto"
 )
 
 const bufSize = 1024 * 1024
 
-var lis *bufconn.Listener
+var listener *bufconn.Listener
 
 func init() {
-	lis = bufconn.Listen(bufSize)
+	listener = bufconn.Listen(bufSize)
 
-	s := grpc.NewServer()
-	pb.RegisterCryptoServiceServer(s, &Server{})
+	serverInstance := grpc.NewServer()
+	
+	pb.RegisterCryptoServiceServer(serverInstance, &Server{})
 
 	go func() {
-		if err := s.Serve(lis); err != nil {
+		if err := serverInstance.Serve(listener); err != nil {
 			log.Fatalf("Error: %v", err)
 		}
 	}()
 }
 
 func bufDialer(context.Context, string) (net.Conn, error) {
-	return lis.Dial()
+	return listener.Dial()
 }
