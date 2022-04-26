c5366ecbb9392809ad635fa156e0a8eac9b5c3ea
Lucas Lima <lucas.lima-95@hotmail.com>
2022-04-25 23:01:36 -0300

    Added client/deleteCrypto.go.

---
+++ client/deleteCrypto.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"log"
+	"context"
+
+	pb "github.com/lucassauro/klever-challenge/src/proto"
+)
+
+func deleteCrypto(client pb.CryptoServiceClient, id uint32) uint32 {
+	if _, err := client.DeleteCrypto(context.Background(), &pb.CryptoId{
+		Id: id,
+	}); err != nil {
+		log.Fatalf("Error")
+	}
+	
+	return id
+}
